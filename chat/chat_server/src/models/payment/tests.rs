9817c137a376a62ea9d79d1cde6f3f9d62e290d4
PY0226H <pyh20010226@gmail.com>
2026-03-08 23:26:58 -0700

    feat: r1-payment-test-import-scope-tightening

--- chat/chat_server/src/models/payment/tests.rs
+++ chat/chat_server/src/models/payment/tests.rs
@@ -2,7 +2,9 @@ use super::receipt_verify::{
     extract_receipt_records, is_retryable_apple_status, normalize_verify_mode,
     select_matching_record, verify_receipt, ReceiptRecord,
 };
-use super::*;
+use super::types::{
+    GetIapOrderByTransaction, ListIapProducts, ListWalletLedger, VerifyIapOrderInput,
+};
 use crate::config::PaymentConfig;
 use crate::{AppError, AppState};
 use anyhow::Result;
